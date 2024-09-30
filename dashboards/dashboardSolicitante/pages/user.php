a8835aca9a7efbabe8a11c3f12105954bd4b611c
AlonsoDev <marcialpebell@gmail.com>
2024-09-29 20:38:33 -0500

    Update link css

--- dashboards/dashboardSolicitante/pages/user.php
+++ dashboards/dashboardSolicitante/pages/user.php
@@ -57,7 +57,7 @@ $apMaterno = $rowSolicitante['apMaterno'];
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
-  <link rel="stylesheet" href="../styles/user.css">
+  <link rel="stylesheet" href="../styles/user_default.css">
   <link href="../../../src/images/Logo.ico" rel="icon">
   <script defer src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
   <script defer src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
